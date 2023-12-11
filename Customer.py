27e5bbb8ab9c2e54deb5a4c7d24bcb192d3df617
alexander <alexandernyaga1999@gmail.com>
2023-12-11 11:58:02 +0300

    Add first and last names

--- Customer.py
+++ Customer.py
@@ -9,5 +9,9 @@ class Customer(Base):
     __tablename__ = 'customers'
 
     id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
     name=Column(String)
     reviews = relationship('Review', back_populates='customer')
+
+    
