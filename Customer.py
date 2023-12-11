15545e99c0262db39674a0483ed35e8801281ca8
alexander <alexandernyaga1999@gmail.com>
2023-12-11 06:32:21 +0300

    Create a customer table

--- Customer.py
+++ Customer.py
@@ -3,3 +3,11 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative  import declarative_base
 
 
+Base = declarative_base
+
+class Customer (Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer, primary_key=True)
+    name=Column(String)
+    reviews = relationship('Review', back_populates='customer')
