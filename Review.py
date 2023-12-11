7bfd25ee22441a1eaae355485b4572dc3ac108fd
alexander <alexandernyaga1999@gmail.com>
2023-12-11 06:41:21 +0300

    Create Reviews table

--- Review.py
+++ Review.py
@@ -1,3 +1,17 @@
 from sqlalchemy import Column,Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+id = Column(Integer, primary_key=True)
+customer_id = Column(Integer, ForeignKey('customers.id'))
+restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+rating = Column(Integer)
+customer = relationship('Customer', back_populates='reviews')
+restaurant = relationship('Restauranr', back_populates='reviews')
+
