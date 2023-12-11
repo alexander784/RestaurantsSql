606894f8a788e68439b5295760a1507e8145c23c
alexander <alexandernyaga1999@gmail.com>
2023-12-11 12:18:23 +0300

    Import review in Customer file

--- Customer.py
+++ Customer.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer,String,ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative  import declarative_base
+from Review import Review
 
 
 Base = declarative_base()
